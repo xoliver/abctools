1ad4b64ce8dd389f9b3ae16944d688fa3dc886ff
Xavier <xoliver@gmail.com>
2013-09-07 18:10:39 +0100

    Basic code in, returning all the ranking chords.

--- abctools.py
+++ abctools.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python
 
+from operator import itemgetter
+import re
 import sys
+
+# TODO
+#   -Tolerate other headers (no big deal)
+#   -Support accidentals (^,^^,=,_,__)
+#
+#
+#
+#
+#
+
+note_finder = re.compile('[a-g](?:\d+|/\d+|/*)')
 
 chords = {
     'C': ('c','e','g'),
@@ -11,7 +24,56 @@ chords = {
     'Am': ('a','c','e')
 }
 
+scale = ['c','d','e','f','g','a','b']
+
+def get_note_duration(note):
+    """
+    Return the duration of a note given as a string based on the note_finder
+    regular expression: N , N/+ , N\d+ , N/\d+
+    """
+
+    if len(note) == 1:
+        return 1.0
+    else:
+        duration = note[1:]
+        if len( duration.replace('/','') ) == 0:
+            return 1.0 / pow(2,len(duration))
+        else:
+            return float(duration)
+
+def find_chords(bar):
+    # Ignore octave and accidentals
+    # TODO don't ignore accidentals
+    bar = bar.translate(None,",'^=_").lower()
+    notes = note_finder.findall(bar)
+    count = 0.0
+    note_count = {}
+
+    #Get count of the given bar, just to try things
+    for note in notes:
+        duration = get_note_duration(note)
+        if note[0] in note_count:
+            note_count[note[0]] += duration
+        else:
+            note_count[note[0]] = duration
+
+    found = {}
+    for note in note_count.keys():
+        for chord in chords.keys():
+            if note in chords[chord]:
+                if chord in found:
+                    found[chord] += note_count[note]
+                else:
+                    found[chord] = note_count[note]
+
+    return found
+
 def load_abc_file(fname):
+    """
+    Read ABC file and return a dictionary containing the different headers plus
+    the tune in the key 'tune'
+    """
+
     abc = {}
     with open(fname, 'r') as f:
         lines = f.readlines()
@@ -34,7 +96,12 @@ def load_abc_file(fname):
     return abc
 
 def extract_bars(tune):
-    bars = tune.replace(':','').replace(' ','').split('|')
+    """
+    Separate a tune into bars ignoring bar playing order indicators and spaces
+    and return an array with them
+    """
+
+    bars = tune.translate(None,': ').split('|')
     for bar in bars:
         if len(bar)==0:
             bars.remove(bar)
@@ -48,7 +115,8 @@ def main(argv):
         sys.exit(0)
     i = 1
     for bar in extract_bars( abc['tune'] ):
-        print i, bar
+        found_chords = find_chords(bar).items()
+        print i, bar, sorted(found_chords, key=itemgetter(1), reverse=True)
         i += 1
     
     #import pdb; pdb.set_trace()
